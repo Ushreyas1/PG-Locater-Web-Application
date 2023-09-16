4e660524f8ce2833032c63f419fb30c4be05735e
Shreyas U <108126277+Ushreyas1@users.noreply.github.com>
2023-09-16 11:38:26 +0530

    Add files via upload

---
+++ db/db.php
@@ -0,0 +1,101 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'paying_guest');
+
+class DB {
+
+    private $con = null;
+
+    function __construct() {
+        $this->con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        if (mysqli_connect_errno()) {
+            throw new Exception("Failed to connect to MySQL: " . mysqli_connect_error());
+        }
+    }
+
+    function __destruct() {
+        $this->close();
+    }
+
+    public function close() {
+        if ($this->con != null) {
+            mysqli_close($this->con);
+            $this->con = null;
+        }
+    }
+
+    public function executeSelect($query) {
+        try {
+            if (!($result = mysqli_query($this->con, $query))) {
+                throw new Exception(mysqli_error($this->con));
+            }
+
+            $results_array = array();
+            while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                $results_array[] = $row;
+            }
+            mysqli_free_result($result);
+            return $results_array;
+        } catch (Exception $ex) {
+            throw $ex;
+        }
+    }
+public function executeSelectrows($query) {
+        try {
+            if (!($result = mysqli_query($this->con, $query))) {
+                throw new Exception(mysqli_error($this->con));
+            }
+            $num = mysqli_num_rows($result);
+            mysqli_free_result($result);
+             return $num;
+        } catch (Exception $ex) {
+            throw $ex;
+        }
+    }
+
+    public function executeInsert($query) {
+        try {
+            if (!mysqli_query($this->con, $query)) {
+                throw new Exception(mysqli_error($this->con));
+            }
+        } catch (Exception $ex) {
+            throw $ex;
+        }
+    }
+
+    public function executeUpdate($query) {
+        $this->executeInsert($query);
+
+        return $this->con->affected_rows;
+    }
+
+    public function executeInsertAndGetId($query) {
+        try {
+            if (!mysqli_query($this->con, $query)) {
+                throw new Exception(mysqli_error($this->con));
+            }
+            return mysqli_insert_id($this->con);
+        } catch (Exception $ex) {
+            throw $ex;
+        }
+    }
+
+    public function executeDelete($query) {
+        $this->executeInsert($query);
+
+        return $this->con->affected_rows;
+    }
+
+    public function escapeString($string) {
+        try {
+            $result = mysqli_real_escape_string($this->con, $string);
+            return $result;
+        } catch (Exception $ex) {
+            throw $ex;
+        }
+    }
+
+}
