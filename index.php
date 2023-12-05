5fe3c3d8e4d6d34e744bf15e713778a2e7b3d106
Shreyas U <108126277+Ushreyas1@users.noreply.github.com>
2023-12-05 12:25:29 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,135 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Paying Guest Information System</title>
+        <link href="static/css/bootstrap.min.css" type="text/css" rel="stylesheet">
+        <link href="static/css/custom.css" type="text/css" rel="stylesheet">
+        <link href="index/css/style.css" type="text/css" rel="stylesheet">
+    </head>
+    <body style="background-image: url('static/images/register.jpg');background-repeat: no-repeat;background-size: cover;background-position: center;">
+        <nav class="navbar navbar-expand-md navbar-dark" style="background-color: red">
+            <a class="navbar-brand font-weight-bold" href="#">Paying Guest Information System</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                <ul class="navbar-nav ml-auto ">
+                    <!--                    <li class="nav-item">
+                                            <a class="nav-link text-white" href="admin/login.php">Admin</a>
+                                        </li>-->
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="owner/login.php">Owner</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="user/login.php">User</a>
+                    </li>
+
+                </ul>
+            </div>
+        </nav>
+
+
+        <div class="container-fluid" style="background-image: url('../static/images/a_home.jpg');background-repeat: no-repeat;background-size: cover;background-position: center;">
+
+            <div class="row justify-content-center mr-5 rounded">
+
+                <div class="col-md-6 mt-5">
+                    <div class="card bg-dark text-white mr-5"  >
+                        <img
+                            src="https://mdbootstrap.com/img/new/slides/017.jpg"
+                            class="card-img"
+                            alt="..."
+                            />
+                        <div class="card-img-overlay">
+
+                            <h3 style="font-family: serif;color: #99ffff;">Paying Guest Information System</h3>
+                            <span>Online Paying Guest Accommodation is a new platform to help people be able to fulfill all their needs for living in a new area. It will benefit the community of students, working people, and therefore the larger community. The web application will be used by the user for various rental purposes like room accommodations, paying guest services, food services, and other day-to-day activities.</span>
+                            <div >
+                                <a  href="./index.php">Read More</a>
+                            </div>
+
+
+                        </div>
+                        <div class="card-footer bg-light text-dark">
+
+                            <span>Demo Store. Mangalore,India</span>
+                            <p>(+91 7022807629)</p>
+                        </div>
+                    </div>
+
+                </div>
+
+
+
+
+                <div class="col-md-4 mt-5">
+
+                    <div class="card ml-5 border-light" style="width: 23rem;">
+                        <div class="about-box">
+
+                            <div class="card-body titlepage">
+                                <h3>About us</h3>
+                                <span>Online Paying Guest Accommodation helps when it is difficult to find rooms to live as paying guests. In the situation of disaffection from the room, you have to find another room and shift there, which is a difficult task for students or any other person. By using this platform , the user will be provided with easy understanding, saving time,rating and feedback panel. It also provides a payment gateway to avoid hassle. </span>
+                                <div >
+                                    <a  href="./index.php">Read More</a>
+                                </div>
+
+                            </div>
+                            <div class="card-footer">
+
+                                <span>Demo Store. Mangalore,India</span>
+                                <p>(+91 7022807629)</p>
+                            </div>
+                        </div></div>
+
+
+                </div>
+            </div>
+        </div>
+        <br><br><br> <br><br><br> <br><br><br>
+        <footer>
+            <div id="contact" class="footer">
+                <div id="contact" class="footer">
+                    <div class="container-fluid">
+                        <div class="row pdn-top-30">
+                            <div class="col-md-12 ">
+                                <div class="footer-box">
+                                    <div class="headinga">
+                                        <h3>Contact us</h3>
+                                        <span>Demo Store. Mangalore,India</span>
+                                        <p>(+91 7022807629)</p>
+   <h5>Mail us<h5>                                  <p><a  href="./index.php">locaterpgrooms@gmail.com</a>
+<p>                             </div>
+                                    <ul class="location_icon">
+                                        <li> <a href="#"><img src="./index/icon/facebook.png"></a></li>
+                                        <li> <a href="#"><img src="./index/icon/Twitter.png"></a></li>
+                                        <li> <a href="#"><img src="./index/icon/linkedin.png"></a></li>
+                                        <li> <a href="#"><img src="./index/icon/instagram.png"></a></li>
+                                    </ul>
+                                    <div class="menu-bottom">
+                                        <ul class="link">
+                                            <li> <a href="admin/login.php">Admin</a></li>
+                                            <li> <a href="user/login.php">User</a></li>
+                                            <li> <a href="supplier/login.php">Supplier</a></li>
+                                            <li> <a href="employee/login.php">Employee</a></li>
+
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="copyright">
+                        <div class="container">
+                            <p>Copyright 2019 All Rights Reserved Design By  <a href="https://html.design/">Free Html Templates</a></p>
+                        </div>
+                    </div>
+                </div></div>
+        </footer>
+
+    </body>
+</html>
---
+++ paying_guest.sql
@@ -0,0 +1,133 @@
+/*
+SQLyog Enterprise - MySQL GUI v8.12 
+MySQL - 5.7.23-log : Database - paying_guest
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`paying_guest1` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `paying_guest`;
+
+/*Table structure for table `booking` */
+
+DROP TABLE IF EXISTS `booking`;
+
+CREATE TABLE `booking` (
+  `book_rid` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `roomId` int(11) DEFAULT NULL,
+  `booked_date` date DEFAULT NULL,
+  `is_paid` varchar(10) DEFAULT 'no' COMMENT 'no-not paid,yes-paid',
+  `booking_status` int(11) DEFAULT '0' COMMENT '0-under process,1-accepted,2-rejected',
+  `payment_type` varchar(20) DEFAULT NULL,
+  `is_vacate` int(5) DEFAULT '0',
+  PRIMARY KEY (`book_rid`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `booking` */
+
+insert  into `booking`(`book_rid`,`user_id`,`roomId`,`booked_date`,`is_paid`,`booking_status`,`payment_type`,`is_vacate`) values (1,1,1,'2021-09-09','yes',1,'upi',1),(2,2,1,'2021-09-09','yes',1,'upi',1),(3,1,2,'2021-09-09','yes',1,NULL,1);
+
+/*Table structure for table `feedback` */
+
+DROP TABLE IF EXISTS `feedback`;
+
+CREATE TABLE `feedback` (
+  `feedback_id` int(5) NOT NULL AUTO_INCREMENT,
+  `user_id` int(5) DEFAULT NULL,
+  `room_id` int(5) DEFAULT NULL,
+  `feedback` text,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`feedback_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `feedback` */
+
+insert  into `feedback`(`feedback_id`,`user_id`,`room_id`,`feedback`,`date`) values (1,1,1,'good','2021-09-09'),(2,2,1,'ghjgj','2021-09-09'),(3,1,2,'gdfgdfgfgfdg','2021-09-09');
+
+/*Table structure for table `gallery` */
+
+DROP TABLE IF EXISTS `gallery`;
+
+CREATE TABLE `gallery` (
+  `gallery_rid` int(11) NOT NULL AUTO_INCREMENT,
+  `room_id` int(11) DEFAULT NULL,
+  `image` blob,
+  `ownerId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`gallery_rid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `gallery` */
+
+insert  into `gallery`(`gallery_rid`,`room_id`,`image`,`ownerId`) values (1,1,'12931140061336de21cbc4.jpg',1),(2,2,'166980281261336df08b230.jpg',1);
+
+/*Table structure for table `owner` */
+
+DROP TABLE IF EXISTS `owner`;
+
+CREATE TABLE `owner` (
+  `owner_rid` int(5) NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) DEFAULT NULL,
+  `contact_no` varchar(12) DEFAULT NULL,
+  `email_id` varchar(40) DEFAULT NULL,
+  `address` varchar(40) DEFAULT NULL,
+  `user_name` varchar(40) DEFAULT NULL,
+  `password` varchar(40) DEFAULT NULL,
+  `is_enabled` int(5) DEFAULT '1' COMMENT '1-enabled,0-disabled',
+  PRIMARY KEY (`owner_rid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `owner` */
+
+insert  into `owner`(`owner_rid`,`name`,`contact_no`,`email_id`,`address`,`user_name`,`password`,`is_enabled`) values (1,'suneetha','8088969962','sun@gmail.com','karkala','sun','1000',1);
+
+/*Table structure for table `room` */
+
+DROP TABLE IF EXISTS `room`;
+
+CREATE TABLE `room` (
+  `room_rid` int(11) NOT NULL AUTO_INCREMENT,
+  `room_num` int(11) DEFAULT NULL,
+  `room_type` varchar(100) DEFAULT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `room_rate` int(11) DEFAULT NULL,
+  `is_booked` varchar(5) DEFAULT 'no',
+  `owner_id` int(5) DEFAULT NULL,
+  `photo` blob,
+  `address` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`room_rid`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `room` */
+
+insert  into `room`(`room_rid`,`room_num`,`room_type`,`description`,`room_rate`,`is_booked`,`owner_id`,`photo`,`address`) values (1,344,'double','A room assigned to two people',78000,'no',1,'158221181261336d9061e72.jpg','Karkala'),(2,4444,'triple','a room assigned to three people',89000,'no',1,'110321148161336dc8af689.jpg','Mangalore'),(3,0,'er','ewr',899,'no',1,'110321148161336dc8af689.jpg','gfdg');
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `user_rid` int(5) NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) DEFAULT NULL,
+  `contact_no` varchar(12) DEFAULT NULL,
+  `email_id` varchar(40) DEFAULT NULL,
+  `address` varchar(40) DEFAULT NULL,
+  `user_name` varchar(40) DEFAULT NULL,
+  `password` varchar(40) DEFAULT NULL,
+  `is_enabled` int(5) DEFAULT '1' COMMENT '1-enabled,0-disabled',
+  PRIMARY KEY (`user_rid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `user` */
+
+insert  into `user`(`user_rid`,`name`,`contact_no`,`email_id`,`address`,`user_name`,`password`,`is_enabled`) values (1,'Neha','9090909090','neha@gmail.com','Karkala','neha','9000',1),(2,'manu','8989898989','manu@gmail.com','karkala','manu','1234',1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
